2d46fee32799b527049bb5c8c18c82eb4282d241
anonymousdomain <domainanonymous1@gmail.com>
2021-03-05 21:04:59 +0300

    add show metod in stack to print all  the value in the stack

--- Stack.java
+++ Stack.java
@@ -1,8 +1,15 @@
 public class Stack {
   int stack[]=new int[5];
+  int top=0;
 public void push(int i){
     stack[0]=data;
+    top++;
 
+}
+public void show(){
+    for(int n:stack){
+        System.out.print(n+" ");
+    }
 }
 
 }
