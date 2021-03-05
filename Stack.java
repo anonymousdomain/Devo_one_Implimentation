93fb2bd984ba8ff1993b06637752d3d261e38441
anonymousdomain <domainanonymous1@gmail.com>
2021-03-05 20:59:53 +0300

    push the first element in the stack

---
+++ Stack.java
@@ -0,0 +1,8 @@
+public class Stack {
+  int stack[]=new int[5];
+public void push(int i){
+    stack[0]=data;
+
+}
+
+}
---
+++ runnur.java
@@ -0,0 +1,6 @@
+public class runnur {
+   public static void main(String[] args) {
+       Stack list=new Stack();
+       list.push(5);
+   } 
+}
