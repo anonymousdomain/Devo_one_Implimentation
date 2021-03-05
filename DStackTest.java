421e7514805f14203d36267334e911393d7865e0
anonymousdomain <domainanonymous1@gmail.com>
2021-03-06 00:17:06 +0300

    test class created

---
+++ DStackTest.java
@@ -0,0 +1,12 @@
+public class DStackTest {
+   public static void main(String[] args) {
+    DStack dlist=new DStack();
+    dlist.push(10);
+    dlist.push(15);
+    dlist.push(12);
+    dlist.show();
+    dlist.push(25);
+    dlist.push(30);
+    dlist.show();
+   } 
+}
