acd18086a4581dd8e68849aed4778ed244578571
anonymousdomain <domainanonymous1@gmail.com>
2021-03-06 00:03:30 +0300

    arraySize intialaized to make the array daynamic & creat a expand array method to copy old arrylist to a new one

---
+++ DStack.java
@@ -0,0 +1,53 @@
+public class DStack {
+int arraySize=3;
+    int stack[] = new int[arraySize];
+    int top = 0;
+  
+    public void push(int data) {
+      if(size()==arraySize)
+        expandArray();
+      
+        
+       else stack[top] = data;
+      top++;
+    private void expandArray() {
+      
+	}
+	}
+  
+    public int pop() {
+      int data=0;
+      if(isEmpty()){
+        System.out.println("the stack is empty");
+      }else{
+       
+        top--;
+        data = stack[top];
+        stack[top] = 0; //delete the value from the top
+        
+      }
+      return data;
+    }
+  
+    public int peek() {
+      int data;
+      data = stack[top - 1];
+      return data;
+    }
+  
+    public int size() {
+      return top;
+    }
+  
+    public boolean isEmpty() {
+      return top <= 0;
+    }
+  
+    public void show() {
+      for (int n : stack) {
+        System.out.print(n + " ");
+      }
+    }
+  }
+
+  
