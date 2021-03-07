b243d92517f1f4591f257dcecf9696415fbd8d0f
anonymousdomain <domainanonymous1@gmail.com>
2021-03-07 22:06:26 +0300

    since we are skiping the front every time we are trying to dequeue,so what we wanna show is' front+i' in the screen

--- Queue.java
+++ Queue.java
@@ -1,17 +1,28 @@
 public class Queue {
-    int arrayS=3;
+    int arrayS=5; 
     int front;
-    
+    int rear;
+    int size;
     int queue[]=new int[arrayS];
     public void enqueue(int data){
         
-        queue[front]=data;
+        queue[rear]=data;
+        rear++;
+        size++;
+    }
+    public int dequeue(){
+        int data;
+        data=queue[front];
         front++;
+        size--;
+        return data;
     }
     public void show(){
-        System.out.println("elements:");
-        for(int i:queue){
-            System.out.print(i+" ");
+        System.out.print("elements:");
+        for(int i=0;i<size;i++){
+            System.out.print(queue[front+i]+" ");
         }
+       System.out.println();
+        
     }
 }
--- QueueTest.java
+++ QueueTest.java
@@ -1,8 +1,12 @@
 public class QueueTest {
  public static void main(String[] args) {
     Queue qlist=new Queue();
+    qlist.enqueue(3);
     qlist.enqueue(5);
     qlist.enqueue(10);
+    qlist.enqueue(12);
+    qlist.show();
+    qlist.dequeue();
     qlist.show();
  }   
 }
