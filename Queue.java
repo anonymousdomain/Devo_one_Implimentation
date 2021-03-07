74df201fc06f4490b25d7114fc2cb5fadffe5d06
anonymousdomain <domainanonymous1@gmail.com>
2021-03-07 22:21:41 +0300

    use moudle to creat a circular array so it will be in loop

--- Queue.java
+++ Queue.java
@@ -7,22 +7,22 @@ public class Queue {
     public void enqueue(int data){
         
         queue[rear]=data;
-        rear++;
+        rear=(rear+1)%5;
         size++;
     }
     public int dequeue(){
         int data;
         data=queue[front];
-        front++;
+        front=(front+1)%5;
         size--;
         return data;
     }
     public void show(){
         System.out.print("elements:");
         for(int i=0;i<size;i++){
-            System.out.print(queue[front+i]+" ");
+            System.out.print(queue[(front+i)%5]+" ");
         }
        System.out.println();
-        
+       
     }
 }
